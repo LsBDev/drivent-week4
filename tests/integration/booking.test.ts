38c1542823a5172450215489025b7712f31fae82
LsB.Dev <prof.luifel@gmail.com>
2023-07-23 20:09:36 -0300

    implementando testes

--- tests/integration/booking.test.ts
+++ tests/integration/booking.test.ts
@@ -11,6 +11,7 @@ import {
   createSession,
   createTicket,
   createTicketType,
+  createTicketTypeWithHotel,
   createUser,
 } from '../factories';
 import { buildBooking } from '../factories/booking-factory';
@@ -22,6 +23,10 @@ beforeEach(async () => {
   await init();
   await cleanDb();
 });
+
+// bookingRouter.get("/booking", getBooking)
+// Status 200 e retorna a reserva com os quartos
+// Status 404 quando não encontra reserva
 
 describe('GET /booking', () => {
   describe('token inválido', () => {
@@ -47,47 +52,99 @@ describe('GET /booking', () => {
     });
   });
 
-  // describe("token válido", () => {
-  //   it("retorna todas as reservas", async () => {
-  //     //criar usuário e reserva
-  //     const user = await createUser()
-  //     const token = generateValidToken(user) // já cria sessão e usuário se não mandar
-  //     const enrollmentWithAddress = await createEnrollmentWithAddress(user)
-  //     const ticketType = await createTicketType()
-  //     const ticket = await createTicket(enrollmentWithAddress.id, ticketType.id, TicketStatus.PAID)
-  //     const hotel = await createHotel()
-  //     const room = await createRoomWithHotelId(hotel.id)
-  //     const booking = await buildBooking(room.id, user.id)
+  describe('token válido', () => {
+    it('retorna todas as reservas', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithHotel();
+      await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+      const hotel = await createHotel();
+      const room = await createRoomWithHotelId(hotel.id);
+      const booking = await buildBooking(room.id, user.id);
 
-  //     const result = await server.get("/booking").set('Authorization', `Bearer ${token}`)
-  //     expect(result.status).toBe(httpStatus.OK)
-  //     expect(result.body).toEqual({
-  //       id: booking.id,
-  //       userId: user.id,
-  //       createAt: booking.createdAt,
-  //       updateAt: booking.updatedAt,
-  //       room: {
-  //         id: room.id,
-  //         name: room.name,
-  //         capacity: room.capacity,
-  //         hotelId: room.hotelId,
-  //         createAt: room.createdAt,
-  //         updateAt: room.updatedAt
-  //       }
-  //     })
-  //   })
-  // })
+      const result = await server.get('/booking').set('Authorization', `Bearer ${token}`);
+      expect(result.status).toBe(httpStatus.OK);
+      expect(result.body).toMatchObject({
+        id: booking.id,
+        Room: {
+          id: room.id,
+          name: room.name,
+          hotelId: room.hotelId,
+          capacity: room.capacity,
+          createdAt: room.createdAt.toISOString(),
+          updatedAt: room.updatedAt.toISOString(),
+        },
+      });
+    });
+  });
 });
-
-// bookingRouter.get("/booking", getBooking)
-// Status 200 e retorna a reserva com os quartos
-// Status 404 quando não encontra reserva
 
 // bookingRouter.post("/booking", "createBooking")
 // Status 200 e retorna {bookingId: id: number}
 // Status 403 para usuário com ticket remoto, que não inclui hotel ou não pago e quando a capacidade do quarto já foi esgotada.
 // Status 404 quando não encontra o quarto com o roomId
 
+describe('POST /booking', () => {
+  describe('token inválido', () => {
+    it('should respond with status 401 if no token is given', async () => {
+      const response = await server.post('/booking');
+
+      expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if given token is not valid', async () => {
+      const token = faker.lorem.word();
+      const response = await server.post('/booking').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if there is no session for given token', async () => {
+      const userWithoutSession = await createUser();
+      const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+      const response = await server.post('/booking').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+  });
+
+  describe('token válido', () => {
+    it('cria a reserva', async () => {
+      const user = await createUser();
+      const token = await generateValidToken(user);
+      const enrollment = await createEnrollmentWithAddress(user);
+      const ticketType = await createTicketTypeWithHotel();
+      await createTicket(enrollment.id, ticketType.id, TicketStatus.PAID);
+    });
+  });
+});
+
 // bookingRouter.update("/booking", updateBooking)
 // Status 403 se não existe reserva no nome do usuário ou a capacidade do quarto foi está esgotada.
 // Status 404 não tem quarto com o id.
+
+describe('PUT /booking/:bookingId', () => {
+  describe('token inválido', () => {
+    it('should respond with status 401 if no token is given', async () => {
+      const response = await server.put('/booking');
+
+      expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if given token is not valid', async () => {
+      const token = faker.lorem.word();
+      const response = await server.put('/booking').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+
+    it('should respond with status 401 if there is no session for given token', async () => {
+      const userWithoutSession = await createUser();
+      const token = jwt.sign({ userId: userWithoutSession.id }, process.env.JWT_SECRET);
+      const response = await server.put('/booking').set('Authorization', `Bearer ${token}`);
+
+      expect(response.status).toBe(httpStatus.UNAUTHORIZED);
+    });
+  });
+});
--- tests/unit/booking.test.ts
+++ tests/unit/booking.test.ts
@@ -2,6 +2,8 @@ import { cleanDb } from '../helpers';
 import bookingRepository from '@/repositories/booking-repository';
 import bookingService from '@/services/booking-service';
 import { init } from '@/app';
+// import enrollmentRepository from '../repositories/enrollment-repository';
+// import ticketsRepository from '../repositories/tickets-repository';
 
 beforeEach(async () => {
   await init();
@@ -9,15 +11,64 @@ beforeEach(async () => {
 });
 
 describe('GET /booking', () => {
-  jest.spyOn(bookingRepository, 'getBooking').mockImplementation((): any => {
-    return null;
-  });
-
   it('Não encontrado', async () => {
+    jest.spyOn(bookingRepository, 'getBooking').mockImplementation((): any => {
+      return null;
+    });
     const promise = bookingService.getBooking(5);
     expect(promise).rejects.toEqual({
       name: 'NotFoundError',
       message: 'No result for this search!',
     });
   });
+
+  it('Retorna a reserva encontrada', async () => {
+    jest.spyOn(bookingRepository, 'getBooking').mockImplementation((userId): any => {
+      return {
+        id: 1,
+        Room: {
+          id: 1,
+          name: 'teste',
+          hotelId: 1,
+          capacity: 4,
+          createdAt: '2023-07-20T09:30:00.000Z',
+          updatedAt: '2023-07-20T09:30:00.000Z',
+        },
+      };
+    });
+    const result = await bookingService.getBooking(5);
+    expect(result).toMatchObject({
+      id: 1,
+      Room: {
+        id: 1,
+        name: 'teste',
+        hotelId: 1,
+        capacity: 4,
+        createdAt: '2023-07-20T09:30:00.000Z',
+        updatedAt: '2023-07-20T09:30:00.000Z',
+      },
+    });
+  });
 });
+
+// describe('POST /booking', () => {
+
+//   afterEach(() => {
+//     jest.clearAllMocks();
+//   });
+
+//   it('should create a booking when all conditions are met', async () => {
+//     // Mock das funções
+//     jest.spyOn(enrollmentRepository, 'findWithAddressByUserId').mockResolvedValue({ id: 1 });
+//     jest.spyOn(ticketsRepository, 'findTicketByEnrollmentId').mockResolvedValue({
+//       TicketType: { isRemote: false, includesHotel: true,
+//       TicketStatus}
+//     });
+//     jest.spyOn(bookingRepository, 'getRoomByRoomId').mockResolvedValue({ id: 1, capacity: 5 });
+//     jest.spyOn(bookingRepository, 'getAllBookingsByRoomId').mockResolvedValue([]);
+
+//     const result = await bookingService.createBooking(1, 1);
+
+//     expect(result).toEqual({ /* ... */ });
+//   })
+// });
